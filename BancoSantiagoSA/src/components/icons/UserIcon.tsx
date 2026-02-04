355cd55b1d239e3c6d941fec154536d4d1964ace
gabrielj <yordivolcano@gmail.com>
2026-02-04 14:36:43 -0400

    BancoSantiagoSA: fix: Corrección de Nombre de atributos en UserIcon

--- BancoSantiagoSA/src/components/icons/UserIcon.tsx
+++ BancoSantiagoSA/src/components/icons/UserIcon.tsx
@@ -7,9 +7,9 @@ export const UserIcon = ({ size, stroke = 'white' }: { size: number, stroke?: st
             viewBox="0 0 24 24"
             fill="none"
             stroke={stroke}
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className="feather feather-user"
         >
             <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
