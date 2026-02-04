b56e4c2384bfa9f7908fcbc4ab81986136e342f1
gabrielj <yordivolcano@gmail.com>
2026-02-04 11:28:58 -0400

    BancoSantiagoSA: feat: Agregando nuevos iconos al Header

---
+++ BancoSantiagoSA/src/components/icons/UserIcon.tsx
@@ -0,0 +1,19 @@
+export const UserIcon = ({ size, stroke = 'white' }: { size: number, stroke?: string }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={size}
+            height={size}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke={stroke}
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="feather feather-user"
+        >
+            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+        </svg>
+    )
+}
--- BancoSantiagoSA/src/components/layout/Header.tsx
+++ BancoSantiagoSA/src/components/layout/Header.tsx
@@ -1,18 +1,30 @@
+import { ChartIcon } from "../icons/ChartIcon"
+import { UserIcon } from "../icons/UserIcon"
+
 export const Header = () => {
     return (
         <header className="w-full p-2.5 py-5 flex flex-wrap gap-3 justify-between items-center">
-            <div>
-                <h1 className="font-bold text-2xl">
+            <div className="flex items-center gap-2">
+                <span className="bg-accent p-2 rounded-md">
+                    <ChartIcon size={20} />
+                </span>
+                <p className="text-xl font-bold">
                     Banco Santiago SA
-                </h1>
+                </p>
             </div>
-            <div className="flex gap-5">
-                <a href="./pages/login.html" className="bg-blue-500 rounded-md p-2 transition-transform hover:scale-104">
+            {/* Pendiente: Añadir ruta */}
+            <a
+                href="#"
+                className="bg-accent flex items-center gap-1 rounded-md p-2 transition-transform hover:scale-104"
+            >
+                <UserIcon size={20} />
+                <span>
                     Iniciar Sessión
-                </a>
-            </div>
+                </span>
+            </a>
         </header>
     )
 }
+
 
 
