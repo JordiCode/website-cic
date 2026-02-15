494957916899609f542e1edd288df5b6f3f067a6
JordiG <yordivolcano@gmail.com>
2026-02-14 22:39:35 -0400

    BancoSantiagoSA: Fix: Corrigiendo error de Atributos de SVG, escribiendolos en formato compatible con tsx

--- BancoSantiagoSA/src/components/icons/ChevronRightIcon.tsx
+++ BancoSantiagoSA/src/components/icons/ChevronRightIcon.tsx
@@ -7,9 +7,9 @@ export const ChevronRightIcon = ({ size, className }: { size: number, className?
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className={className}
         >
             <polyline points="9 18 15 12 9 6"></polyline>
