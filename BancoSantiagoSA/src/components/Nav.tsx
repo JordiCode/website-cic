c7e8ebc1a7eb71ad0dc2d2f7ae1c3712ae7bcb6e
JordiG <yordivolcano@gmail.com>
2026-02-25 14:44:43 -0400

    BancoSantiagoSA: refactor: Agregando Link a Nav.tsx

--- BancoSantiagoSA/src/components/Nav.tsx
+++ BancoSantiagoSA/src/components/Nav.tsx
@@ -1,30 +1,44 @@
-export const Nav = ({ className }: { className?: string }) => {
+import { Link } from "react-router"
+
+export const Nav = ({ className, toggleOpen }: { className?: string, toggleOpen: () => void }) => {
     return (
         <nav className={className}>
             <ul className="flex max-md:flex-col text-baseText ">
-
-                <li className="hover:text-primaryText hover:bg-accent/50 active:bg-accent/50 rounded-md">
-                    <a href="#" className="p-2 py-4 block">Contáctanos  </a>
+                <li className="hover:text-primaryText hover:bg-accent/40 active:bg-accent/50 rounded-md">
+                    <Link
+                        to="/contactanos"
+                        className="p-2 py-4 block"
+                        onClick={toggleOpen}
+                    >
+                        Contáctanos
+                    </Link>
                 </li>
-                <li className="hover:text-primaryText hover:bg-accent/50 active:bg-accent/50 rounded-md">
-                    <a href="#" className="p-2 py-4 block">Guía de Ayuda</a>
+                <li className="hover:text-primaryText hover:bg-accent/40 active:bg-accent/50 rounded-md">
+                    <Link
+                        to="/guia-de-ayuda"
+                        className="p-2 py-4 block"
+                        onClick={toggleOpen}
+                    >
+                        Guía de Ayuda
+                    </Link>
                 </li>
             </ul>
 
             <div className="flex max-md:flex-col gap-2">
-                <a
-                    href="#"
+                <Link
+                    to="/hazte-cliente"
                     className="border border-baseColor  text-baseText hover:text-primaryText px-4 py-2 rounded-2xl hover:bg-accent/50 active:bg-accent/50"
+                    onClick={toggleOpen}
                 >
                     Hazte cliente
-                </a>
-
-                <a
-                    href="#"
+                </Link>
+                <Link
+                    to="/iniciar-sesion"
                     className="bg-secondaryColor  text-secondaryText px-4 py-2 rounded-2xl hover:bg-secondaryColor/90 active:bg-secondaryColor/90"
+                    onClick={toggleOpen}
                 >
                     Iniciar Sesión
-                </a>
+                </Link>
 
             </div>
         </nav>
