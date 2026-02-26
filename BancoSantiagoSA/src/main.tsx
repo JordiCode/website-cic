e97991bdb9174487208d80213231aac73617e637
JordiG <yordivolcano@gmail.com>
2026-02-25 21:17:28 -0400

    BancoSantiagoSA: feat: Agregando nuevas rutas en main.tsx

--- BancoSantiagoSA/src/main.tsx
+++ BancoSantiagoSA/src/main.tsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Layout from './Layout.tsx'
 import { Home } from './pages/Home.tsx'
-import { NotFound } from './pages/NotFound.tsx'
+import { LogginAccount } from './pages/LoginAccount.tsx'
+import { MaintenancePage } from './pages/MaintenancePage.tsx'
+import { AuthProvider } from './context/AuthContext.tsx'
+import { Dashboard } from './pages/Dashboard.tsx'
+import { ProtectedRoute } from './components/ProtectedRoute.tsx'
 
 const router = createBrowserRouter([
   {
@@ -11,13 +15,23 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { index: true, element: <Home />, },
-      { path: "*", element: <NotFound /> },
+      { path: 'iniciar-sesion', element: <LogginAccount /> },
+      { path: "*", element: <MaintenancePage /> },
     ],
   },
+  {
+    path: '/dashboard',
+    element:
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>,
+  }
 ])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
