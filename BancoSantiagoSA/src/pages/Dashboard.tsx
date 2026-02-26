5e4e907be6a45314bdd24e3ab58ff96c9588b322
JordiG <yordivolcano@gmail.com>
2026-02-25 21:29:30 -0400

    BancoSantiagoSA: fix: Mensaje mal escrito

--- BancoSantiagoSA/src/pages/Dashboard.tsx
+++ BancoSantiagoSA/src/pages/Dashboard.tsx
@@ -8,7 +8,7 @@ export const Dashboard = () => {
         <div className="text-baseText max-w-7xl mx-auto mt-16 pt-20 text-center">
             <LogoIcon size={50} className="text-accent/80 mb-12 mx-auto" />
             <h2 className="text-primaryText text-2xl text-center mb-6">{user?.name} </h2>
-            <p className="text-primaryText  text-center mb-12">Te haz loggeado exitosamente ✅️</p>
+            <p className="text-primaryText  text-center mb-12">Te has loggeado exitosamente ✅️</p>
             <p onClick={logout} className="cursor-pointer text-accent hover:brightness-110">
                 Cerrar sesión
             </p>
