c3f8b7ee46c6ed4a7d60bce79d839cd5882a1204
JordiG <yordivolcano@gmail.com>
2026-02-26 18:57:11 -0400

    BancoSantiagoSA: refactor: Actualizando Home y quitando prop size en piggyBankIcon.tsx

--- BancoSantiagoSA/src/pages/Home.tsx
+++ BancoSantiagoSA/src/pages/Home.tsx
@@ -1,8 +1,5 @@
-//import { FAQ } from "../components/FAQ"
 import { FAQ } from "../components/FAQ"
 import { Hero } from "../components/Hero"
-import { Footer } from "../components/layout/Footer"
-//import { Location } from "../components/Location"
 import { Services } from "../components/Services"
 import { Support } from "../components/Support"
 
@@ -13,7 +10,6 @@ export const Home = () => {
             <Services />
             <Support />
             <FAQ />
-            {/* <Location /> */}
         </>
     )
 }
